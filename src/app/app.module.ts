7f307534fc2abc2723e8d7f1498609a78cf838ae
David Hernandez <=>
2023-08-07 22:40:39 -0600

    signup page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { environment } from '../environments/environment';
 import { StoreModule } from '@ngrx/store';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -23,11 +25,13 @@ import { AuthService } from './services/auth.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     StoreModule.forRoot({}, {}),
+    ToastrModule.forRoot(),
 
   ],
   providers: [AuthService, { provide: FIREBASE_OPTIONS, useValue: environment.firebase }],
